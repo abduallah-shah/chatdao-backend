bd57ad13d3f0ce421e7c63d7eda655c3df37ab0e
abdullah-shah <linkabdullah9@gmail.com>
2025-06-01 15:15:39 +0500

    refactor: update CORS origin configuration and simplify vercel.json routes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.set("trust proxy", 1);
 // cors setup
 app.use(
   cors({
-    origin: process.env.FRONT_URL || "http://localhost:3000" || "https://chat-dao.vercel.app",
+    origin: process.env.FRONT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
